66f1fbabd7722865e854b95227d791c21abc4a51
Daniel <daniel.n.griswold@gmail.com>
2024-07-24 14:03:28 -0400

    Initial Commit

---
+++ MiniAccountTests/MiniAccountTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniAccountTests.swift
+//  MiniAccountTests
+//
+//  Created by Daniel on 7/24/24.
+//
+
+import Testing
+@testable import MiniAccount
+
+struct MiniAccountTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
